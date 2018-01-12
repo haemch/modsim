291d0c8f6bfcc249f3acb78c7846d9ebb208e7df
Christoph Hämmerle <Christoph@ChristophsMBP.local>
2018-01-12 15:22:25 +0100

    Task 2 und Task 3

--- simulation.m
+++ simulation.m
@@ -1,3 +1,5 @@
+%Task 1
+
 % duration
 months = 120;
 % schrittweite der diskretisierung? 
@@ -27,7 +29,7 @@ result= zeros(3,months);
 result(1,1)=1;
 result(2,1)=x0;
 result(3,1)=y0;
-
+figure
 for i=2:months
     
     result(1,i)=i;
@@ -47,6 +49,122 @@ for i=2:months
     result(3,i)= y_new;
     
 end
+subplot(3,1,1)
 plot(result(1,:), result(2,:), result(1,:), result(3,:));
 legend('deer','fox')
+
+
+%Task 2
+
+rows = 70;
+col = 200;
+k=1;
+p1= 0.01;
+p2= 0.8;
+p3= 0.01;
+
+result_ca= zeros(3,months);
+result_ca(1,1)=1;
+result_ca(2,1)=x0;
+result_ca(3,1)=y0;
+
+%init a matix of rows*col cells with x0 deers and y0 foxes at random
+%positions
+% 0 ... empty
+% 1 ... deer
+% 2 ... fox
+N = rows*col;
+random_positions = randperm(N, y0 + x0);
+init_values = zeros(1,N);
+for i=1:(y0+x0)
+    if i<=x0
+        init_values(random_positions(i))=1;
+    else
+        init_values(random_positions(i))=2;
+    end
+   
+end
+ca = vec2mat(init_values, col);
+
+ 
+
+%simulation of ca
+for m=1:months
+    tmp=ca;
+    for i=1:rows
+        for j=1:col
+            
+            %if cell is empty
+            if ca(i,j) == 0
+                
+                neighbour = getRandomNeighbour(ca,k,i,j);
+                
+                %check if neighbour is prey
+                if neighbour == 1
+                    
+                    %if rand <= p1 empty cell is prey
+                    if rand()<= p1
+                        tmp(i,j) = 1;
+                    end
+                end
+                
+            %if cell is prey    
+            elseif ca(i,j) == 1
+             neighbour = getRandomNeighbour(ca,k,i,j);
+                
+                %check if neighbour is predator
+                if neighbour == 2
+                    
+                    %if rand <= p2 empty cell is predator
+                    if rand()<= p2
+                        tmp(i,j) = 2;
+                    end
+                end
+                
+            %if cell is predator
+            elseif ca(i,j) == 2
+                
+                %if rand <= p3 predator is dying
+                if rand() <= p3
+                    tmp(i,j) = 0;
+                end
+            end
+   
+        end
+    end
+    ca=tmp;
+    
+    subplot(3,1,2)
+    hold on;
+    spy(ca==0, 'w');
+    spy(ca==2,'r');
+    spy(ca==1,'g');
+    hold off;
+    drawnow
+    
+    result_ca(1,m)=m;
+    result_ca(2,m)= sum(sum(ca==1));
+    result_ca(3,m)= sum(sum(ca==2));
+    
+end
+% add plot of aggregated ca
+subplot(3,1,3)
+plot(result_ca(1,:), result_ca(2,:), result_ca(1,:), result_ca(3,:));
+legend('deer','fox')
+
+%return random neighbour 
+function neighbour = getRandomNeighbour(ca,k,x,y)
+neighbours= [];
+rows=size(ca,1);
+col=size(ca,2);
+for i=(x-k):(x+k)
+    for j= (y-k):(y+k)
+        if i>0 && i<=rows && j>0 && j<=col 
+          neighbours = [neighbours;ca(i,j)];  
+        end
+    end
+end
+
+neighbour = neighbours(randperm(numel(neighbours),1));
+end
 
