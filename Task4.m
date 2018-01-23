ec6e3c14fb05f61c07b1d51381c59032917f0256
Philipp Ganiu <pganiu@gmail.com>
2018-01-23 17:54:00 +0100

    minor change

--- Task4.m
+++ Task4.m
@@ -1,8 +1,10 @@
 x0 = 4000;
 y0 = 1000;
 months = 120;
+rows = 70;
+col = 200;
 
-result_ca = Task2_3(x0, y0, months, 70, 200, 1, 0.01, 0.99, 0.01);
+result_ca = Task2_3(x0, y0, months, rows, col, 1, 0.01, 0.99, 0.01);
 
 figure(1);
 plot(result_ca(1,:), result_ca(2,:), result_ca(1,:), result_ca(3,:));
