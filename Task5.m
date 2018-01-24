23bb2eb8510468915202afab3ba9e99f39034418
Christoph Hämmerle <Christoph@e248-198.eduroam.tuwien.ac.at>
2018-01-24 15:33:10 +0100

    pictures and parameters

--- Task5.m
+++ Task5.m
@@ -1,15 +1,23 @@
 x0 = 4000;
 y0 = 1000;
-months = 120;
+months = 60;
 
 
 result = Task1(x0, y0, months,0.1,10000,0.001, 2, 0.003, 0.001);
-result_ca = Task2_3(x0, y0, months, 70, 200, 1, 0.01, 0.99, 0.01);
+result_ca = Task2_3(x0, y0, months, 50, 200, 2, 0.44, 0.99, 0.25);
 
-figure(1);
-plot(result(1,:), result(2,:), result(1,:), result(3,:),result_ca(1,:), result_ca(2,:), result_ca(1,:), result_ca(3,:));
-legend('deer','fox','deer_ca','fox-ca');
+
+subplot(1,2,1)
+plot(result(1,:), result(2,:), result(1,:), result(3,:));
+legend('deer','fox');
 grid on;
 ylabel('population number');
 xlabel('months');
-axis([0 120 0 6000]);
+axis([0 months 0 6000]);
+subplot(1,2,2)
+plot(result_ca(1,:), result_ca(2,:), result_ca(1,:), result_ca(3,:));
+legend('deer ca','fox ca');
+grid on;
+ylabel('population number');
+xlabel('months');
+axis([0 months 0 6000]);
