52cab49f061f550d335822d04295485718217db9
Christoph Hämmerle <Christoph@e248-198.eduroam.tuwien.ac.at>
2018-01-24 16:14:10 +0100

    task 4

--- Task4.m
+++ Task4.m
@@ -1,26 +1,26 @@
-x0 = 40;
-y0 = 10;
-months = 120;
-rows = 2;
-col = 50;
-rows2 = 10;
-col2 = 10;
+x0 = 4000;
+y0 = 1000;
+months = 60;
+rows = 20;
+col = 500;
+rows2 = 100;
+col2 = 100;
 
 result_ca = Task2_3(x0, y0, months, rows, col, 2, 0.44, 0.99, 0.25);
-result_ca2 = Task2_3(x0, y0, months, rows, col,2, 0.44, 0.99, 0.25);
+result_ca2 = Task2_3(x0, y0, months, rows2, col2,2, 0.44, 0.99, 0.25);
 
-subplot(1,2,1)
+subplot(2,1,1);
 plot(result_ca(1,:), result_ca(2,:), result_ca(1,:), result_ca(3,:));
 legend('deer','fox');
 grid on;
 ylabel('population number');
 xlabel('months');
-axis([0 120 0 6000]);
+axis([0 months 0 6000]);
 
-subplot(1,2,2)
+subplot(2,1,2);
 plot(result_ca2(1,:), result_ca2(2,:), result_ca2(1,:), result_ca2(3,:));
 legend('deer','fox');
 grid on;
 ylabel('population number');
 xlabel('months');
-axis([0 120 0 6000]);
+axis([0 months 0 6000]);
